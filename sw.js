54e75430b4c944f81428972ab7d92982f59dc993
yanoshijapan <146075607+yanoshijapan@users.noreply.github.com>
2026-05-12 11:06:18 +0700

    Modify service worker for caching and logo updates
    Updated cache name and added logo URLs. Implemented cache activation and deletion of old caches.

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'yanoshi-dashboard-v3';
+const CACHE_NAME = 'yanoshi-dashboard-v1';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  'https://yanoshijapan.github.io/asetonline/logoappyanoshi.png' /* <--- Link logo dimasukkan ke sini */
+  'https://yanoshijapan.github.io/asetonline/logohitam.png',
+  'https://yanoshijapan.github.io/asetonline/logoappyanoshi.png'
 ];
 
 // Install Service Worker
@@ -11,18 +12,34 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// Fetch dari Cache atau Jaringan
+// Fetching (Gunakan cache jika offline)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
+        // Return response dari cache, atau fetch dari network
         return response || fetch(event.request);
       })
+  );
+});
+
+// Aktivasi dan hapus cache lama
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
