583a96fb3b89a9404ecee250c44a2c9a75559642
yanoshijapan <146075607+yanoshijapan@users.noreply.github.com>
2026-07-18 12:02:04 +0700

    Update CACHE_NAME to version v15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yanoshi-dashboard-v14'; 
+const CACHE_NAME = 'yanoshi-dashboard-v15'; 
 
 const urlsToCache = [
   'index.html',     // Ganti dari './index.html' menjadi 'index.html'
