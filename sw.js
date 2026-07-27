6f7fc597be89a3dd8eb44db7bdd45dbc36e5297d
yanoshijapan <146075607+yanoshijapan@users.noreply.github.com>
2026-07-27 10:04:11 +0700

    Change CACHE_NAME to 'yanoshi-dashboard-v24'
    Updated cache name for service worker to version 24.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yanoshi-dashboard-v23'; 
+const CACHE_NAME = 'yanoshi-dashboard-v24'; 
 
 const urlsToCache = [
   'index.html',     // Ganti dari './index.html' menjadi 'index.html'
