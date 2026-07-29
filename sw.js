7b3e6a01992dd94ea5dcdd9ef1f53a012e16925f
yanoshijapan <146075607+yanoshijapan@users.noreply.github.com>
2026-07-29 12:00:06 +0700

    Change CACHE_NAME to 'yanoshi-dashboard-v25'
    Updated cache name to version 25 and modified URLs.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yanoshi-dashboard-v24'; 
+const CACHE_NAME = 'yanoshi-dashboard-v25'; 
 
 const urlsToCache = [
   'index.html',     // Ganti dari './index.html' menjadi 'index.html'
