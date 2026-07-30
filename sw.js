2e1f1bd16b227905d1ac3647da70921bf4ef032a
yanoshijapan <146075607+yanoshijapan@users.noreply.github.com>
2026-07-30 11:55:57 +0700

    Update CACHE_NAME and cache URLs in sw.js
    Updated cache name from v29 to v30 and modified URLs.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yanoshi-dashboard-v29'; 
+const CACHE_NAME = 'yanoshi-dashboard-v30'; 
 
 const urlsToCache = [
   'index.html',     // Ganti dari './index.html' menjadi 'index.html'
