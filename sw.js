da528d24556881274aec244fedcd5e317e33149f
yanoshijapan <146075607+yanoshijapan@users.noreply.github.com>
2026-08-15 14:07:08 +0700

    Update cache name to yanoshi-dashboard-v37

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yanoshi-dashboard-v35'; 
+const CACHE_NAME = 'yanoshi-dashboard-v37'; 
 
 const urlsToCache = [
   'index.html',     // Ganti dari './index.html' menjadi 'index.html'
