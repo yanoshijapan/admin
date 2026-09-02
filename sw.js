59ba05260393bf656814f6001e73232bb91814b5
yanoshijapan <146075607+yanoshijapan@users.noreply.github.com>
2026-09-02 12:01:44 +0700

    Update service worker cache name and URLs
    Updated CACHE_NAME to version v53 and modified cached URLs.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yanoshi-dashboard-v51'; 
+const CACHE_NAME = 'yanoshi-dashboard-v53'; 
 
 const urlsToCache = [
   'index.html',     // Ganti dari './index.html' menjadi 'index.html'
