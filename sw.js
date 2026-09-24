39c6c432b0f2207cb418cf56f893ef248a1cb5a7
yanoshijapan <146075607+yanoshijapan@users.noreply.github.com>
2026-09-24 09:53:19 +0700

    Update CACHE_NAME to version 65
    Updated cache name from 'yanoshi-dashboard-v63' to 'yanoshi-dashboard-v65'.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yanoshi-dashboard-v63'; 
+const CACHE_NAME = 'yanoshi-dashboard-v65'; 
 
 const urlsToCache = [
   'index.html',     // Ganti dari './index.html' menjadi 'index.html'
