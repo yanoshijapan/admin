a3dcec073d8bc48a38524176ed3ef95f64aa2c43
yanoshijapan <146075607+yanoshijapan@users.noreply.github.com>
2026-09-26 14:47:33 +0700

    Update CACHE_NAME to version v68

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yanoshi-dashboard-v67'; 
+const CACHE_NAME = 'yanoshi-dashboard-v68'; 
 
 const urlsToCache = [
   'index.html',     // Ganti dari './index.html' menjadi 'index.html'
