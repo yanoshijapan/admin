c7d4a79c61d8873474c579c1ece4cfcee1128579
yanoshijapan <146075607+yanoshijapan@users.noreply.github.com>
2026-09-26 14:31:32 +0700

    Increment CACHE_NAME to v66 and update cached URLs
    Updated CACHE_NAME to version 66 and modified cache URLs.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yanoshi-dashboard-v65'; 
+const CACHE_NAME = 'yanoshi-dashboard-v66'; 
 
 const urlsToCache = [
   'index.html',     // Ganti dari './index.html' menjadi 'index.html'
